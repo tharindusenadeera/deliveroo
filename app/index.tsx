7b7b8eae104df0b4d806d65b0852afdc3d36e70b
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-07 22:50:14 +0100

    examined the routes between screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      <Link href="/details">View details</Link>
     </View>
   );
 }
