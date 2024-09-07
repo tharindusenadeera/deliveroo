a52ffe81b1857ef857ec653b677d2991aa71350c
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-07 23:19:41 +0100

    Removed unwanted heades

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="(home)" />
       <Tabs.Screen name="settings" />
     </Tabs>
