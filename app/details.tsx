cc32dd5075467cb4af11818559bd4c3a8f6c5e77
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-07 22:48:02 +0100

    Verifying the route functionality

--- app/details.tsx
+++ app/details.tsx
@@ -1,10 +1,9 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+export default function DetailsScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Details</Text>
     </View>
   );
 }
