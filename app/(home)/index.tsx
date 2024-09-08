8e001290a334c38cc2a9ea06b22e1ba61a417867
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-08 23:31:04 +0100

    Header design done

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,25 +1,29 @@
 import { Text, View, SafeAreaView, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
+import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/outline';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: 'http://links.papareact.com/wru',
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
-        </View>
-        <View>
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: 'http://links.papareact.com/wru',
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-          <Text className="font-bold text-xl">Current Location</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
-      </Text>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
     </SafeAreaView>
   );
 }
