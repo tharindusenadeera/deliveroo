55ca88f7e58828954e1ad6abe6f6a1fcb554da46
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-12 22:04:27 +0100

    Category card implemented

--- components/home/CategoryCard.tsx
+++ components/home/CategoryCard.tsx
@@ -1,10 +1,21 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
 
-const CategoryCard = () => {
+interface CategoryCard {
+  imgUrl: string;
+  title: string;
+}
+
+const CategoryCard = ({ imgUrl, title }: CategoryCard) => {
   return (
-    <View>
-      <Text>Category Card</Text>
-    </View>
+    <TouchableOpacity className="relative mr-2">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="h-20 w-20 rounded"
+      />
+      <Text className="absolute bottom-1 left-1 text-white font-bold">{title}</Text>
+    </TouchableOpacity>
   );
 };
 
