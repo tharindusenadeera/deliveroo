95ee12ed0c04af95f1b5da504ddbed8204a55606
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-14 23:01:12 +0100

    Restaurant card styles done

--- components/home/RestaurantCard.tsx
+++ components/home/RestaurantCard.tsx
@@ -1,10 +1,55 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { MapPinIcon } from 'react-native-heroicons/outline';
+import { StarIcon } from 'react-native-heroicons/solid';
 
-const RestaurantCard = () => {
+interface RestaurantCard {
+  id: number;
+  imgUrl: string;
+  title: string;
+  rating: number;
+  genre: string;
+  address: string;
+  shortDescription: string;
+  dishes: any;
+  long: number;
+  lat: number;
+}
+
+const RestaurantCard: React.FC<RestaurantCard> = ({
+  id = null,
+  imgUrl = '',
+  title = '',
+  rating = null,
+  genre = '',
+  address = '',
+  shortDescription = '',
+  dishes = [],
+  long = null,
+  lat = null,
+}) => {
   return (
-    <View>
-      <Text>Restaurant Card</Text>
-    </View>
+    <TouchableOpacity className="bg-white mr-3 shadow">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="h-36 w-64 rounded-sm"
+      />
+      <View className="px-3 pb-4">
+        <Text className="font-bold text-lg pt-2">{title}</Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text className="text-xs text-gray-500">
+            <Text className="text-green-500">{rating}</Text>. {genre}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center space-x-1">
+          <MapPinIcon color="gray" opacity={0.4} size={22} />
+          <Text className="text-xs text-gray-500">Nearby . {address}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
