f6f621645f1622cd55ea33f03874cc74992bc6fb
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-09-16 10:18:02 +0100

    Sanity configuration done

--- sanity.ts
+++ sanity.ts
@@ -1,12 +1,12 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const client = {
+const client = createClient({
   projectId: 'oe8urrcu',
   dataset: 'production',
   useCdn: true,
   apiVersion: '2024-10-15',
-};
+});
 
 const builder = imageUrlBuilder(client);
 export const urlFor = (source: any) => builder.image(source);
